cd3831f5c47c79e05a504d4c735c0db3351714fc
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-06-26 21:31:27 -0500

    shows hard-coded movie times

--- config/movie_loader.rb
+++ config/movie_loader.rb
@@ -1,3 +1,36 @@
+# require 'nokogiri'
+# require 'open-uri'
+# require 'json'
+
+# class MovieLoader
+#   def compile_todays_list
+#     # Grabs info from internet
+#     list_page = Nokogiri::HTML(open('http://www.fandango.com/amcrivereast21_aaqow/theaterpage'))
+#     collection = []
+
+#     list_page.css('div.showtimes-movie-container').each do |link|
+
+#       title = link.css('a.showtimes-movie-title').first.content
+#       rating_time = link.css('div.showtimes-movie-rating-runtime').first.content
+#       rating_time_stripped = rating_time.gsub(/\n\s+(\b|\z)/,'')
+#       rating_time_array = rating_time_stripped.split(" , ")
+#       time = rating_time_array.last
+#       rating = rating_time_array.first
+
+#       collection << {
+#                       title: title,
+#                       time: time,
+#                       rating: rating
+#                       }
+#     end
+
+#     # writes to file, ruby terminal app
+#     File.open("movie_list.json","w") do |f|
+#       f.write(collection.to_json)
+#     end
+#   end
+# end
+
 require 'nokogiri'
 require 'open-uri'
 require 'json'
@@ -5,17 +38,13 @@ require 'json'
 class MovieLoader
   def compile_todays_list
     # Grabs info from internet
-    list_page = Nokogiri::HTML(open('http://www.fandango.com/amcrivereast21_aaqow/theaterpage'))
+    list_page = Nokogiri::HTML(open('https://www.amctheatres.com/movie-theatres/chicago/amc-river-east-21'))
     collection = []
 
-    list_page.css('div.showtimes-movie-container').each do |link|
-
-      title = link.css('a.showtimes-movie-title').first.content
-      rating_time = link.css('div.showtimes-movie-rating-runtime').first.content
-      rating_time_stripped = rating_time.gsub(/\n\s+(\b|\z)/,'')
-      rating_time_array = rating_time_stripped.split(" , ")
-      time = rating_time_array.last
-      rating = rating_time_array.first
+    list_page.css('.PosterContent').each do |element|
+      title = element.css('h3').first.content
+      time = element.css('.u-separator.js-runtimeConvert.u-inlineFlexCenter').first.content
+      rating = element.css('.Headline--eyebrow p span')[3].content
 
       collection << {
                       title: title,
--- controllers/scheduler_controller.rb
+++ controllers/scheduler_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../config/ruby_manifest.rb'
+require 'json'
 
 class SchedulerController
   attr_reader :load_view, :runner_view, :movie_loader, :today
@@ -22,13 +23,58 @@ class SchedulerController
       load_view.finished
     end
 
+    file = File.read('movie_list.json')
+    data_hash = JSON.parse(file)
+    movies = data_hash
+    index = 0
+    movies.each do |title|
+      p movies[index]['title']
+      index += 1
+    end
     until finished
       # logic for your program
 
-      runner_view.ask_if_user_is_finished
-      user_choice = gets.chomp
+      standard_movie_times = []
+      standard_movie_end_times = []
+      t = Time.new(2018, 06, 26, 11, 30)
+      t2 = t + 7200
 
-      if user_choice == 'end'
+      
+
+
+      standard_movie_times << t.strftime('%I:%M %p')
+      standard_movie_end_times << t2.strftime('%I:%M %p')
+      three_d_movie_times = ["11:00"]
+      
+
+      runner_view.ask_if_user_is_finished
+      picked_movie = gets.chomp
+
+      index = 0
+      movies.length.times do
+        if picked_movie == movies[index]['title']
+          runner_view.three_d_question
+
+          version = gets.chomp.downcase
+          if version == "standard"
+            p movies[index]['title']
+            p movies[index]['rating']
+            p movies[index]['time']
+            p movies[index]['theatre']
+            p standard_movie_times
+            p standard_movie_end_times
+            break
+          else 
+            p three_d_movie_times
+            break
+          end
+        else
+         index += 1
+        end
+      end
+
+
+      if picked_movie == 'end'
         exit_program 
       end
     end    
